3c8a06ec3e555d4f1cb8813c88a6ec1d34b8a35a
Dawson De Mond <dragon520111@gmail.com>
2025-06-24 09:55:05 -0400

    Removed code and left classes without implementation.

--- Homework/Homework 1/src/io/Output.java
+++
@@ -1,7 +0,0 @@
-package io;
-
-import java.util.List;
-
-public interface Output {
-    void writeLines(List<Line> lines);
-}
--- Homework/Homework 1/src/processing/Sorter.java
+++
@@ -1,9 +0,0 @@
-package processing;
-
-import io.Line;
-
-import java.util.List;
-
-public interface Sorter {
-    List<Line> sortLines(List<Line> lines);
-}
