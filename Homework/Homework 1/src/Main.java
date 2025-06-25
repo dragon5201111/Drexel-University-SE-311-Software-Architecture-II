2cb93d8685fc607ce8f03cdf9ea659de6c263628
Dawson De Mond <dragon520111@gmail.com>
2025-06-25 11:55:44 -0400

    Minor modification.

--- Homework/Homework 1/src/Main.java
+++ Homework/Homework 1/src/Main.java
@@ -6,7 +6,10 @@ import process.Indexer;
 
 public class Main {
     public static void main(String[] args) {
-        String filename = "C:\\Users\\drago\\IdeaProjects\\Drexel-University-SE-311-Software-Architecture-II\\Homework\\Homework 1\\test\\lines.txt";
+        String filename =   "C:\\Users\\drago\\" +
+                            "IdeaProjects\\Drexel-University-SE-311-Software-Architecture-II\\" +
+                            "Homework\\Homework 1\\" +
+                            "test\\lines.txt";
         LineStorage lineStorage = new LineStorage();
 
         Input input = new TextInput(lineStorage);
