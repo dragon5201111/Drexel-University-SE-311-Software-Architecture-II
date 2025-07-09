baab9ab1c2a66bea800ca398ae3b8fd9667713ab
Dawson De Mond <dragon520111@gmail.com>
2025-07-09 13:21:14 -0400

    Added files.

--- Homework/Homework 2/src/Main.java
+++ Homework/Homework 2/src/Main.java
@@ -1,3 +1,6 @@
+import io.Input;
+import resources.OptionReader;
+
 public class Main {
     public static void main(String[] args) {
 
---
+++ Homework/Homework 2/src/resources/KWICObjectLoader.java
@@ -0,0 +1,21 @@
+package resources;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class KWICObjectLoader extends ClassLoader {
+
+	public Object loadObject(String className) {
+     try {
+            ClassLoader loader = this.getClass().getClassLoader();
+            Class<?> aClass = loader.loadClass(className);
+            Constructor<?> constructor = aClass.getConstructor();
+            return constructor.newInstance();
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException |
+                 InvocationTargetException | NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+        return null;
+	}
+
+}
