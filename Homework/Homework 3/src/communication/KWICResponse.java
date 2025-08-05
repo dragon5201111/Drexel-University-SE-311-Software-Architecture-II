c49d29a1b8239f1068d6ecb91a0a73d979e7a496
Dawson De Mond <dragon520111@gmail.com>
2025-08-05 10:03:27 -0400

    Minor update.

--- Homework/Homework 3/src/communication/KWICResponse.java
+++ Homework/Homework 3/src/communication/KWICResponse.java
@@ -1,4 +1,15 @@
 package communication;
 
+import java.util.List;
+
 public class KWICResponse {
+    private final List<String> message;
+
+    public KWICResponse(List<String> message) {
+        this.message = message;
+    }
+
+    public List<String> getMessage() {
+        return this.message;
+    }
 }
--- Homework/Homework 3/src/server/ClientHandler.java
+++ Homework/Homework 3/src/server/ClientHandler.java
@@ -1,4 +1,7 @@
 package server;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
